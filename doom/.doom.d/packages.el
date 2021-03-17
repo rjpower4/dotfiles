fecadae8ae6d0699b563acca5de1683d705e0fc1
Rolfe Power <rolfepower4@gmail.com>
2021-03-17 15:10:47 -0400

    disable solaire mode

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -37,6 +37,9 @@
 ;; you can do so here with the `:disable' property:
 ;(package! builtin-package :disable t)
 
+(package! solaire-mode
+  :disable t)
+
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
 ;; from Doom or MELPA/ELPA/Emacsmirror:
