39a24dcb552b73d63e784858b526b4dd2f1f2518
Rolfe Power <rolfepower4@gmail.com>
2021-05-08 13:24:49 -0400

    Add scripts folder

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ for config_group in $standard; do
     stow --dotfiles -t $HOME $config_group
 done
 
-# --- Config group
+# --- Config, Local groups
 # --- This is the group that needs to go in $HOME/.config
 # --- We have to do this because of https://github.com/aspiers/stow/issues/33
 stow --dotfiles -t $HOME/.config config
+stow --dotfiles -t $HOME/.local local
