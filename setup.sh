7a9e93b56eaa57ec9e702aec6b4bfcfd0f8f4ef1
Rolfe Power <rolfepower4@gmail.com>
2021-05-11 15:44:50 -0400

    Move home configs into home directory

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 # ---  Standard Config Groups
-standard="shell tmux"
-for config_group in $standard; do
-    stow --dotfiles -t $HOME $config_group
-done
+#standard="shell tmux"
+#for config_group in $standard; do
+#    stow --dotfiles -t "$HOME" "$config_group"
+#done
+
+function stowHome {
+    local hpath="./home"
+    for config_group in "$hpath"/*; do
+        if [[ -d "$config_group" && ! -L "$config_group" ]]; then
+            stow --dotfiles -d "$hpath" -t $HOME $(basename "$config_group")
+        fi
+    done
+}
 
 # --- Config, Local groups
 # --- This is the group that needs to go in $HOME/.config
 # --- We have to do this because of https://github.com/aspiers/stow/issues/33
-stow --dotfiles -t $HOME/.config config
-stow --dotfiles -t $HOME/.local local
+stow --dotfiles -t "$HOME/.config" config
+stow --dotfiles -t "$HOME/.local" local
+
+stowHome
