ac94188d0ea2a68e06a7211e3245e1c5372fa660
Rolfe Power <rolfepower4@gmail.com>
2021-05-28 00:11:59 -0400

    Organize config

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,106 +1,127 @@
 ;;; $DOOMDIR/config.el  --- Personal Configuration -*- lexical-binding: t; -*-
 ;;
+;;  Author: Rolfe Power
+;;
 ;;; Commentary:
+;;
+;; This file contains the my personal configuration for Doom Emacs. To see what
+;; packages are being installed, see `init.el' in this same directory. In this
+;; file, I set constants, define my own functions, and modify the configuration
+;; of several of the packages installed.
 ;;
 ;;; Code:
 
-;; --- Contact information
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------- PERSONAL INFORMATION --------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (setq user-full-name       "Rolfe Power"
       user-mail-address    (rot13 "ebysrcbjre4@tznvy.pbz"))
 
-;; --- Check system type
-(defconst *sys/linux*
-  (eq system-type 'gnu/linux))
+;; -------------------------------------------------------------------------- ;;
+;; -------------------------- SYSTEM INFORMATION ---------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
+(defconst *sys/linux* (eq system-type 'gnu/linux))  ; Linux (Well, actually...)
+(defconst *sys/win32* (eq system-type 'windows-nt)) ; Windows (Why? God, Why?)
+(defconst *sys/macos* (eq system-type 'darwin))     ; MacOS (Hail Tim Apple)
 
-(defconst *sys/win32*
-  (eq system-type 'windows-nt))
+(defconst *sys/root* (string-equal "root" (getenv "USER"))) ; Are we root?
 
-(defconst *sys/win32*
-  (eq system-type 'darwin))
+(defconst *sys/gui* (display-graphic-p)) ; Do we have a GUI?
 
-;; --- Are we root?
-(defconst *sys/root*
-  (string-equal "root" (getenv "USER")))
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------- FINDING EXECUTABLES ---------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
+;; These are earmuffed variables that give the paths of some potentially use- ;;
+;; ful programs. This is primarily to use in user-defined functions or config ;;
+;; options.                                                                   ;;
+;; -------------------------------------------------------------------------- ;;
+(defconst *clangd* (executable-find "clangd"))     ; LLVM C/C++ Language Server
+(defconst *gcc* (executable-find "gcc"))           ; Gnu C Compiler
+(defconst *git* (executable-find "git"))           ; Git
+(defconst *pdflatex* (executable-find "pdflatex")) ; LaTeX Building Tool
+(defconst *python3* (executable-find "python3"))   ; Python 3 Executable
+(defconst *python* (executable-find "python"))     ; Default System Python
+(defconst *rg* (executable-find "rg"))             ; Ripgrep (grep alternative)
+(defconst *tr* (executable-find "tr"))             ; Translate/Delete Char Tool
 
-;;; --- We on a GUI
-(defconst *sys/gui*
-  (display-graphic-p))
+;; -------------------------------------------------------------------------- ;;
+;; --------------------------- BASIC SETTINGS ------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
+(setq-default delete-by-moving-to-trash t)         ; Non-committal about it
+(setq-default uniquify-buffer-name-style 'forward) ; Show path to uniquify
+(setq inhibit-compacting-font-caches t)            ; Keep fonts, costs memory
+(global-subword-mode 1)                            ; Iterate through CamelCase
+(setq initial-scratch-message                      ; How nice...
+      ";; Welcome to emacs!\n\n")
+(add-hook 'compilation-filter-hook                 ; Keep Comint buffer within
+          'comint-truncate-buffer)                 ; reason... i.e.,
+(setq comint-buffer-maximum-size 2000)             ; under 2000 lines
 
-;; --- Finding Executables
-(defconst *clangd*
-  (or (executable-find "clangd")
-      (executable-find "/usr/local/opt/llvm/bin/clangd")))
-
-(defconst *gcc*
-  (executable-find "gcc"))
-
-(defconst *git*
-  (executable-find "git"))
-
-(defconst *pdflatex*
-  (executable-find "pdflatex"))
-
-(defconst *python3*
-  (executable-find "python3"))
-
-(defconst *python*
-  (executable-find "python"))
-
-(defconst *rg*
-  (executable-find "rg"))
-
-(defconst *tr*
-  (executable-find "tr"))
-
-;; Basic Settings
-(setq-default delete-by-moving-to-trash t
-              tab-width 4
-              uniquify-buffer-name-style 'forward)
-(setq inhibit-compacting-font-caches t)
-(global-subword-mode 1) ;; Iterate through CamelCaseWords
-
-;; --- Directories
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------- DIRECTORY SETTINGS ----------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (setq power/home-directory "/home/rpower/")
 (setq power/org-directory (concat power/home-directory "Dropbox/org"))
 
-;; --- Visual Settings
-(setq doom-font (font-spec :size 15))
-;;      doom-variable-pitch-font (font-spec :family "Roboto")
-;;      doom-serif-font (font-spec :family "Roboto"))
+;; -------------------------------------------------------------------------- ;;
+;; --------------------------- VISUAL SETTINGS ------------------------------ ;;
+;; -------------------------------------------------------------------------- ;;
+(setq display-line-numbers-type t)      ; Line Numbers
+(setq confirm-kill-emacs nil)           ; Don't Confirm Exit
+(display-time-mode 1)                   ; Show Time in the Mode Line
+(setq-default tab-width 4)              ; 1 Tab == 4 Spaces
+(add-hook 'prog-mode-hook               ; Gray Line @ 80
+          'display-fill-column-indicator-mode)
+(setq-default fill-column 80)           ; I need to see multiple windows...
 
-;; Line Numbers
-(setq display-line-numbers-type t)
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------------ LOCATION ---------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
+(setq calendar-latitude   40.424660)    ; Horizontal Lines...
+(setq calendar-longitude -86.848460)    ; Vertical Lines...
 
-;; Thin grey line separating windows
-(set-face-background 'vertical-border "grey")
-(set-face-foreground 'vertical-border (face-background 'vertical-border))
-
-;; Location
-(setq calendar-latitude 40.424660)
-(setq calendar-longitude -86.848460)
-
-;; --- Themes
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------------- THEMING ---------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (use-package! modus-themes
-  :config
-  (setq modus-themes-no-mixed-fonts t
-        modus-themes-org-blocks 'grayscale
-        modus-themes-slanted-constructs t))
-(setq power/light-theme   'modus-operandi)
-(setq power/dark-theme    'modus-vivendi)
-(setq power/default-theme 'power/light-theme)
+  :custom
+  (modus-themes-bold-constructs t)
+  (modus-themes-completions 'nil)
+  (modus-themes-no-mixed-fonts t)
+  (modus-themes-diffs 'desaturated)
+  (modus-themes-fringes 'intense)
+  (modus-themes-lang-checkers 'colored-background)
+  (modus-themes-links nil)
+  (modus-themes-mail-citations nil)
+  (modus-themes-mode-line nil)
+  (modus-themes-org-blocks nil)
+  (modus-themes-paren-match 'intense-bold)
+  (modus-themes-prompts 'intense-gray)
+  (modus-themes-region 'bg-only-no-extend)
+  (modus-themes-scale-headings nil)
+  (modus-themes-syntax 'yellow-comments-green-strings)
+  (modus-themes-variable-pitch-headings nil)
+  (modus-themes-variable-pitch-ui nil)
+  (modus-themes-slanted-constructs t))
+
+(setq power/light-theme   'modus-operandi)    ; Default Light Theme
+(setq power/dark-theme    'modus-vivendi)     ; Default Dark Theme
+(setq power/default-theme 'power/light-theme) ; Default Theme
 
 (defun power/night-mode ()
+  "Set the theme to the night-mode (dark) variant."
   (interactive)
   (load-theme power/dark-theme t)
   (doom/reload-theme))
 
 (defun power/day-mode ()
+  "Set the theme to the day-mode (light) variant."
   (interactive)
   (load-theme power/light-theme t)
   (doom/reload-theme))
 
 (defun power/auto-theme ()
-  "Select the theme automatically based on the time of day."
+  "Select the theme automatically based on the sunrise/sunset time."
   (require 'solar)
   (let* ((cur-hour (string-to-number (substring (current-time-string) 11 13)))
          (sun-events (solar-sunrise-sunset (calendar-current-date)))
@@ -113,106 +134,73 @@
 (after! modus-themes
   (power/auto-theme))
 
-;; --- Misc
-;; Show time in mode line
-(display-time-mode 1)
-
-;; --- Org
+;; -------------------------------------------------------------------------- ;;
+;; ---------------------------------- ORG ----------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (setq org-directory power/org-directory)
 (use-package! org
   :mode ("\\.org\\'" . org-mode)
   :init
-  (setq org-catch-invisible-edits 'show))
-
-(after! org
-  ;; Clear Doom's default templates
-  (setq org-capture-templates '())
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "PROG(p)" "WAIT(w)" "QUES(q)"
+  (setq org-catch-invisible-edits 'show)
+  :custom
+  (org-capture-templates '())
+  (org-todo-keywords
+        '((sequence "TODO(t)" "PROG(p)" "WAIT(w)"
                     "|"
-                    "DONE(d)" "DEFR(f)" "CANC(c)")))
-  (setq org-todo-keyword-faces
+                    "DONE(d)" "CANC(c)")))
+  (org-todo-keyword-faces
         '(("TODO" . "red")
           ("PROG" . "goldenrod")
           ("WAIT" . "orchid")
-          ("QUES" . "salmon")
           ("DONE" . "forest green")
-          ("DEFR" . "steel blue")
           ("CANC" . "dark gray"))
         )
-  (setq org-confirm-babel-evaluate nil
-        org-use-property-inheritance t
-        org-export-use-babel nil
-        org-startup-indented t
-        org-pretty-entities nil
-        org-outline-path-complete-in-steps nil)
-  (setq org-agenda-files (list org-directory (concat org-directory "journal/"))))
+  (org-confirm-babel-evaluate nil)
+  (org-use-property-inheritance t)
+  (org-export-use-babel nil)
+  (org-startup-indented t)
+  (org-pretty-entities nil)
+  (org-outline-path-complete-in-steps nil)
+  (org-agenda-files (list org-directory (concat org-directory "journal/"))))
 
 
 ;; Org Journal
 (use-package! org-journal
   :after org
   :config
-  (customize-set-variable 'org-journal-dir (expand-file-name "journal/" org-directory))
+  (customize-set-variable 'org-journal-dir
+                          (expand-file-name "journal/" org-directory))
   (customize-set-variable 'org-journal-file-format "%Y-%m-%d.org")
   (customize-set-variable 'org-journal-date-format "%e %b %Y (%A)")
   (customize-set-variable 'org-journal-date-prefix "#+TITLE: ")
   (customize-set-variable 'org-journal-time-prefix "* ")
   (customize-set-variable 'org-journal-time-format ""))
 
-;; Pomodoro
-(setq org-pomodoro-keep-killed-pomodoro-time 1)
-
-;; -- Dired
+;; -------------------------------------------------------------------------- ;;
+;; -------------------------------- DIRED ----------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (after! dired
   (setq dired-listing-switches "-aBhl  --group-directories-first"))
 
-;; --- Python
-(after! python
-  (setq python-shell-interpreter "python3"))
-
-;; --- Latex
-(after! latex (setq font-latex-fontify-script nil))
-
-;; --- Avy
-
-;; Custom Bindings for jumping
-(use-package! avy
-  :custom
-  (avy-all-windows t)
-  :config
-  (map! :leader
-        :desc "Word"   "j" #'avy-goto-word-or-subword-1
-        :desc "Window" "W" #'ace-window
-        :desc "Line"   "J" #'avy-goto-line))
-
-;; --- Ace window
+;; -------------------------------------------------------------------------- ;;
+;; ----------------------------- ACE WINDOW --------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (use-package! ace-window
   :config
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (map! :leader
-        "w w" 'ace-window))
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))) ; Set keys to home row (easy)
 
-;; --- Swiper
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------------- SWIPER ----------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (setq swiper-use-visual-line nil)
 (setq swiper-use-visual-line-p (lambda (_) nil))
 
-;; --- Clangd
-(setq lsp-clients-clangd-args '("-j=3"
-                                "--background-index"
-                                "--clang-tidy"
-                                "--completion-style=detailed"
-                                "--header-insertion=never"))
-(after! lsp-clangd (set-lsp-priority! 'clangd 2))
-
-;; Misc
-(setq confirm-kill-emacs nil)
-(add-hook 'compilation-filter-hook 'comint-truncate-buffer)
-(setq comint-buffer-maximum-size 2000)
-
-;; History
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------------- HISTORY ---------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 (use-package! recentf
   :custom
+  (recentf-max-saved-items 10000)
   (recentf-exclude '((expand-file-name package-user-dir)
                      ".cache"
                      ".cask"
@@ -231,10 +219,13 @@
       `(not julia-mode
             text-mode))
 
-;; Evil
+;; -------------------------------------------------------------------------- ;;
+;; -------------------------------- EVIL ------------------------------------ ;;
+;; -------------------------------------------------------------------------- ;;
 (use-package! evil
   :defer t
   :custom
+  ;; I like seeing the box all the time...
   (evil-normal-state-cursor  'box)
   (evil-insert-state-cursor  'box)
   (evil-visual-state-cursor  'hollow)
@@ -242,16 +233,28 @@
   (evil-replace-state-cursor  'hbar)
   (evil-operator-state-cursor 'evil-half-cursor))
 
-;; Show a message
-(setq initial-scratch-message ";; Welcome to emacs!\n\n")
+;; -------------------------------------------------------------------------- ;;
+;; ------------------------------ LANGUAGES --------------------------------- ;;
+;; -------------------------------------------------------------------------- ;;
 
-;; Show the fill column
-(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
-(setq fill-column 90)
+;; -------------------------------- C/C++ ----------------------------------- ;;
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
-;; Julia
+;; -------------------------------- JULIA ----------------------------------- ;;
 (use-package! julia-repl
   :config
   (julia-repl-set-terminal-backend 'vterm))
 
-(setq recentf-max-saved-items 10000)
+;; -------------------------------- LATEX ----------------------------------- ;;
+(after! latex
+  (setq font-latex-fontify-script nil))
+
+
+;; ------------------------------- PYTHON ----------------------------------- ;;
+(after! python
+  (setq python-shell-interpreter "python3"))
